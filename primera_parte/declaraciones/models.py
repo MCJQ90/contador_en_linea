5ae22f31f48052cb9e5e63347a82a36ce1999687
MCJQ90 <mariocar14@hotmail.com>
2025-07-24 11:46:20 -0500

    migrado

--- primera_parte/declaraciones/models.py
+++ primera_parte/declaraciones/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -10,12 +11,41 @@ class Cliente(models.Model):
     email = models.EmailField()
     telefono = models.CharField(max_length=15)
 
-class Declaracion(models.Model):
+class SolicitudServicio(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tipoServicio = models.CharField(max_length=50)
+    añofiscal = models.IntegerField()
+    fechaSolicitud = models.DateField(default=timezone.now)
+    estado = models.CharField(max_length=20, choices=[('finalizada', 'Finalizada'), ('asistente_asignado', 'Asistente Asignado'), 
+                                                      ('Pendiente', 'Pendiente'), ('en_proceso', 'En proceso')], default='Pendiente')
+    observaciones = models.TextField(blank=True)
+
+class AsistenteContable(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    especialidad = models.CharField(max_length=50)
+    estado = models.CharField(max_length=20, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')], default='Activo')
+
+class AsignacionAsistente(models.Model):
+    solicitudServicio = models.ForeignKey(SolicitudServicio, on_delete=models.CASCADE)
+    usuarioAsignado = models.ForeignKey(AsistenteContable, on_delete=models.CASCADE)
+    fechaAsignacion = models.DateField(auto_now_add=True)
+
+class Declaracion(models.Model):
+    solicitud = models.ForeignKey(SolicitudServicio, on_delete=models.CASCADE)
     tipoDeclaracion = models.CharField(max_length=50)
     añofiscal = models.IntegerField()
     ingresos = models.DecimalField(max_digits=10, decimal_places=2)
     egresos = models.DecimalField(max_digits=10, decimal_places=2)
-    estado = models.CharField(max_length=20, choices=[('Pendiente', 'Pendiente'), ('en_revision', 'En revisión'),
-            ('enviada', 'Enviada'), ('Aprobada', 'Aprobada'), ('Rechazada', 'Rechazada')], default='Pendiente')
-    
+    estado = models.CharField(max_length=20, choices=[
+        ('Pendiente', 'Pendiente'),
+        ('en_revision', 'En revisión'),
+        ('enviada', 'Enviada'),
+        ('Aprobada', 'Aprobada'),
+        ('Rechazada', 'Rechazada')
+    ], default='Pendiente')
+    valorImpuesto = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    fechaPresentacion = models.DateField(null=True, blank=True)
+
