3c9c7ae5974ab586059a57e6de4b42a8721af059
MCJQ90 <mariocar14@hotmail.com>
2025-07-25 16:50:43 -0500

    revisaar

--- primera_parte/declaraciones/urls.py
+++ primera_parte/declaraciones/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('listar_clientes/', views.listar_clientes, name='listar_clientes'),
     path('<str:numero_identificacion>', views.detalle_cliente, name='detalle_cliente'),
+    path('formulario_cliente/', views.formulario_cliente, name='formulario_cliente'),
 ]
